a563223be9d4e57adf65be467f8256e99e3304ab
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-04-29 01:30:42 +0430

    reading hostname

--- 06. Hostname/06._Hostname/06._Hostname.ino
+++ 06. Hostname/06._Hostname/06._Hostname.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
+String newHostname = "MyNodeMCU";
+
+void setup() {
+  Serial.begin(9600);
+  delay(5000);
+  WiFi.mode(WIFI_STA);
+
+  //Get Current Hostname
+  Serial.printf("Default hostname: %s\n", WiFi.hostname().c_str());
+
+  //Set new hostname
+  WiFi.hostname(newHostname.c_str());
+
+  //Get Current Hostname
+  Serial.printf("New hostname: %s\n", WiFi.hostname().c_str());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
