2d518131a060a18d0519fc2dbd658c4efef72357
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-04-29 01:45:32 +0430

    connecting to wi-fi as a station

--- 07. Station_Mode/07._Station_Mode/07._Station_Mode.ino
+++ 07. Station_Mode/07._Station_Mode/07._Station_Mode.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
+const char* ssid = "Dellink";
+const char* password = "wtkh-daah-y8bj";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+
+  // Scanning Wi-Fis
+  int numberOfNetworks = WiFi.scanNetworks();
+ 
+  for(int i = 0; i < WiFi.scanNetworks(); i++){
+      Serial.print("Network name: ");
+      Serial.println(WiFi.SSID(i));
+      Serial.println("-----------------------");
+  }
+  
+  // Init Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(5000);
+  }
+  Serial.print("\nIP Address: ");
+  Serial.print(WiFi.localIP());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
