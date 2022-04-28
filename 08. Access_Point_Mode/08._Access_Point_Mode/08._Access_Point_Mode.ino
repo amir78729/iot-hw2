b1263c37de0c987a1f2abb6cdc1426145c7d5126
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-04-29 02:15:09 +0430

    adding access point mode

--- 08. Access_Point_Mode/08._Access_Point_Mode/08._Access_Point_Mode.ino
+++ 08. Access_Point_Mode/08._Access_Point_Mode/08._Access_Point_Mode.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
+IPAddress local_IP(10, 10, 10, 1);
+IPAddress gateway(10, 10, 10, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(3000);
+
+  WiFi.softAP("ESP8266-AP");
+
+  Serial.print("IP address (Before Cofig): ");
+  Serial.println(WiFi.softAPIP());
+
+  Serial.print("Setting configuration ... ");
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  
+  Serial.print("IP address (After Config): ");
+  Serial.println(WiFi.softAPIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
