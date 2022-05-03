4104a10e4e30903feab34235f61a767207291b0c
Amirhossein Alibakhshi <amirhosseinalibakhshi@gmail.com>
2022-05-03 21:21:47 +0430

    add parking to repo

--- 10._Parkingdoor/10._Parkingdoor.ino
+++ 10._Parkingdoor/10._Parkingdoor.ino
@@ -1,67 +1,92 @@
+#include "ESP8266WiFi.h"
+#include "ESP8266WebServer.h"
+#include "webpage.h"
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-const int TRIGGER_PIN = 12; // assiging trigger pin for ultrasonic sensor: D6
-const int ECHO_PIN = 14; // assiging echo pin for ultrasonic sensor: D5
-const int SERVO_PIN = 0; // assiging servo pin: D3
-const int LED_PIN = 2; // assiging servo pin: D4
+#define LED 2
+#define SERVO 0
+#define TRIGGER_PIN 4
+#define ECHO_PIN 5
+#define BUZZER_PIN 16
 
-const int TIMER_DURATION = 5000;
-
-//define sound velocity in cm/uS
 #define SOUND_VELOCITY 0.034
+bool flag = true;
 
-long duration;
-float distance_in_cm;
-bool is_target_detected = false;
-bool is_open = false;
-int timer_millies = -1;
+ESP8266WebServer server(80);
+const String DOOR_IS_CLOSED = "DOOR_IS_CLOSED";
+const String DOOR_IS_OPEN = "DOOR_IS_OPEN";
+const String OPENING_DOOR = "OPENING_DOOR";
+const String CLOSING_DOOR = "CLOSING_DOOR";
+const String OBJECT_DETECTED = "OBJECT_DETECTED";
+
+const char *ssid = "Dellink";
+const char *password = "wtkh-daah-y8bj";
+bool is_connected = false;
+String doorState = DOOR_IS_CLOSED;
+String command = "wait";
+int open_time = 0;
+String alert_text = "";
+int alert_time = 0;
 int pos = 0;
+bool is_object_detected;
+Servo myservo;
 
-void setup() {
-  myservo.attach(SERVO_PIN);  // attaching servo to the servo object
-  Serial.begin(9600);
-  pinMode(TRIGGER_PIN, OUTPUT); // Sets the TRIGGER_PIN as an Output
-  pinMode(ECHO_PIN, INPUT); // Sets the ECHO_PIN as an Input
-  pinMode(LED_PIN, OUTPUT); // Sets the ECHO_PIN as an Input
-  myservo.write(pos);
+void webpage() {
+  server.send(200, "text/html", webpage_html);
 }
 
-void loop() {
-  distance_in_cm = get_distance_in_cm();
-  
-  if (distance_in_cm > 10 ) { // no object was detected closer than 10cm
-    is_target_detected = false;
-    if (is_open) {
-      close_the_door();
-    }
-    
-  } else { // an object was detected closer than 10cm
-    
-    if (!is_target_detected) { // the object was appeared recently
-      
-      Serial.println("the object was appeared recently");
-      is_target_detected = true;
-      timer_millies = millis();
-      
-    } else {
-      
-      if (millis() > timer_millies + TIMER_DURATION) { // the timer has been finished and we can open the door
-        
-        if (!is_open) {
-          open_the_door();
-        }
-        
-      } else { // we should wait for timer...
-        wait_for_timer();
-      }
-    }
+void button() {
+  server.send(200, "text/plain", "clicked");
+  command = server.arg("state");
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+void alert() {
+  server.send(200, "text/plain", alert_text);
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  pinMode(LED, OUTPUT);
+  myservo.attach(SERVO);
+  myservo.write(0);
+  pinMode (TRIGGER_PIN, OUTPUT);  //ultrasonic sensor output pin
+  pinMode (ECHO_PIN, INPUT); //ultrasonic sensor input pin
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
   }
-  digitalWrite(LED_PIN, is_target_detected ? HIGH : LOW);
+
+  Serial.println();
+  if (WiFi.status() == WL_CONNECTED){
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+    is_connected = true;
+  }
+
+  if (is_connected) {
+    server.on("/", webpage);
+    server.on("/button", button);
+    server.on("/alert", alert);
+    server.onNotFound(handle_NotFound);
+    server.begin();
+    Serial.println("Parking's server is up");
+  } else {
+    Serial.println("Connection failed");
+  }
 }
 
 float get_distance_in_cm() {
-  
+  long duration;
   // Clears the TRIGGER_PIN
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
@@ -69,32 +94,90 @@ float get_distance_in_cm() {
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
-
   // Reads the ECHO_PIN, returns the sound wave travel time in microseconds
   duration = pulseIn(ECHO_PIN, HIGH);
-
   // return the distance
   return duration * SOUND_VELOCITY / 2;
 }
 
-void close_the_door() {
-  Serial.println("closing the door");
+void open_door() {
+  digitalWrite(LED, HIGH);
+  while (pos < 180) {
+    myservo.write(pos);
+    delay(15);
+    pos += 1;
+  }
+  open_time = millis();
+  digitalWrite(LED, LOW);
+  doorState = DOOR_IS_OPEN;
+  alert_text = DOOR_IS_OPEN;
+  command = "wait";
+}
+void close_door() {
+  
+  alert_text = CLOSING_DOOR;
+  if (flag) {
+    flag = false;
+    delay(100);
+    return;
+  }
+  flag = true;
+  Serial.println("Closing the door...");
   for (pos = 180; pos >= 0; pos -= 1) {
+    if (get_distance_in_cm() < 20) {
+      Serial.println("Object was detected");
+      alert_text = OBJECT_DETECTED;
+      return;
+    }
     myservo.write(pos);
+    delay(15);
   }
-  is_open = false;
+  doorState = DOOR_IS_CLOSED;
+  alert_text = DOOR_IS_CLOSED;
+  command = "wait";
 }
 
-void open_the_door() {
-  Serial.println("the timer has been finished and we can open the door");
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    myservo.write(pos);
-  }
-  is_open = true;
-}
 
-void wait_for_timer() {
-  Serial.print("waiting for timer...(");
-  Serial.print(TIMER_DURATION - (millis() - timer_millies));
-  Serial.println("ms remaining)");
+void loop() {
+  server.handleClient();
+  if (alert_text == OBJECT_DETECTED) {
+    alert_text = OBJECT_DETECTED;
+    if (flag) {
+      digitalWrite(BUZZER_PIN, HIGH);
+      flag = false;
+      delay(500);
+      digitalWrite(BUZZER_PIN, LOW);
+      return;
+    }
+    flag = true;
+    open_door();
+  }
+  if (command == "o" ) {
+    if (doorState == DOOR_IS_CLOSED) {
+      
+      alert_text = OPENING_DOOR;
+      if (flag) {
+        flag = false;
+        delay(100);
+        return;
+      }
+      Serial.println("Opening the door...");
+      flag = true;
+      open_door();
+      return;
+    }
+  }
+  if (command == "c" && doorState == DOOR_IS_OPEN) {
+    close_door();
+    return;
+  }
+  if (command == "wait") {
+    alert_text = doorState;
+    if (doorState == DOOR_IS_OPEN) {
+      if (millis() - open_time >= 20000) {
+        close_door();
+      }
+    }
+
+  }
 }
---
+++ 10._Parkingdoor/webpage.h
@@ -0,0 +1,93 @@
+const char webpage_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html lang="en">
+ <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Parking Door</title>
+    <style>
+        body {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        section {
+          width: 350px;
+        }
+        @media screen and (max-width: 450px) {
+            section {
+                max-width: 90vw;
+            }
+        }
+    </style>
+  </head>
+<body>
+  <section>
+    <h1 class="display-3 text-center mb-3">Parking Door</h1>
+    <hr>
+    <div class="container mb-3">
+            <div class="row justify-content-between">
+                <button class="btn btn-success col-5" onclick="processButton(event)" id="open">OPEN</button>
+                 <button class="btn btn-danger col-5" onclick="processButton(event)" id="close">CLOSE</button>
+            </div>
+        </div>
+    
+    <div id="status-container" class="alert alert-light text-center">
+          <p id="status"></p>
+    </div>
+  </section>
+</body>
+<script>
+const resonseInfo = {
+  DOOR_IS_CLOSED: {
+      message: 'Door is Closed.',
+      type: 'info'
+  },
+  DOOR_IS_OPEN: {
+      message: 'Door is Open.',
+      type: 'info '
+  },
+  OPENING_DOOR: {
+      message: 'Opening the Door...',
+      type: 'warning '
+  },
+  CLOSING_DOOR: {
+      message: 'Closing the Door...',
+      type: 'warning'
+  },
+  OBJECT_DETECTED: {
+      message: 'An object was detected! Re-Opning the door...',
+      type: 'danger'
+  },
+}
+function processButton(e){
+  console.log("sending request");
+  var target = e.target || window.target;
+  console.log(target);
+  console.log(target.id);
+  let buttonState = target.id;
+  var xhttp = new XMLHttpRequest();
+  
+  xhttp.open("GET", "button?state="+buttonState, false);
+  xhttp.send();
+}
+
+setInterval(() => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = () => {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = xhttp.responseText;
+      document.getElementById("status").innerHTML = resonseInfo[response]?.message || 'Something went wrong! Please try again';
+      document.getElementById("status-container").className = `alert alert-${resonseInfo[response]?.type || 'danger'} text-center`;
+    }
+  };
+  xhttp.open("GET", "alert");
+  xhttp.send();
+}, 100);
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</html>
+)rawliteral";
